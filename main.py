cad9964b41e94d8bd79a1fe4cf402e2704eb5b39
Ashot <avhmail@gmail.com>
2024-10-11 00:59:40 +0200

    test request

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
 from flask import Flask
+import requests
 import os
 app = Flask(__name__)
 
 @app.route('/response')
 def response():
-    return "hello"
+    response = requests.get("http://188.185.78.106:5000/latest/cryostat")
+    return response.json()
+    
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8080, debug=False)
